8df9272e846a5774b4aaab735d3d909de55cb47b
edsonstudio <edsont8@hotmail.com>
2023-12-27 14:36:40 -0300

    Adicionados parametros de Rotas

--- src/backoffice/controllers/customer.controller.ts
+++ src/backoffice/controllers/customer.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Delete, Get, Post, Put } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
 
 @Controller('v1/customers')
 export class CustomerController {
@@ -7,18 +7,26 @@ export class CustomerController {
         return 'Obter os clientes';
     }
 
+    @Get(':document')
+    getById(@Param('document') document) {
+        return 'Obter um cliente por id(document): ' + document;
+    }
+
     @Post()
-    post() {
-        return 'Criar um cliente';
+    post(@Body() body) {
+        return body;
     }
 
-    @Put()
-    put() {
-        return 'Atualizar um cliente';
+    @Put(':document')
+    put(@Param('document') document, @Body() body) {
+        return {
+            customer: document,
+            data: body
+        };
     }
 
-    @Delete()
-    delete() {
+    @Delete(':document')
+    delete(@Param('document') document) {
         return 'Remover um cliente';
     }
 }
