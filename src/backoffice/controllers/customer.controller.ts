c8569ee67aa97bbc877cf1fb717868683f2b0da5
edsonstudio <edsont8@hotmail.com>
2023-12-27 14:24:52 -0300

    adicionado nome na rota e os decorators dos verbos http em cada metodo da controller

--- src/backoffice/controllers/customer.controller.ts
+++ src/backoffice/controllers/customer.controller.ts
@@ -1,19 +1,23 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Delete, Get, Post, Put } from "@nestjs/common";
 
-@Controller()
+@Controller('v1/customers')
 export class CustomerController {
+    @Get()
     get() {
         return 'Obter os clientes';
     }
 
+    @Post()
     post() {
         return 'Criar um cliente';
     }
 
+    @Put()
     put() {
         return 'Atualizar um cliente';
     }
 
+    @Delete()
     delete() {
         return 'Remover um cliente';
     }
