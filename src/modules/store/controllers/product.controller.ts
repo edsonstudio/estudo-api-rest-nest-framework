7d25563333adabc5b71ce0a26eb449930d9d1063
edsonstudio <edsont8@hotmail.com>
2023-12-29 13:46:42 -0300

    Criados os endpoints para o CRUD de produto

--- src/modules/store/controllers/product.controller.ts
+++ src/modules/store/controllers/product.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Get, HttpException, HttpStatus } from "@nestjs/common";
+import { Body, Controller, Delete, Get, HttpException, HttpStatus, Param, Post, Put } from "@nestjs/common";
 
 import { ProductService } from "../services/product.service";
 
 import { ResultDto } from "src/shared/dtos/result.dto";
+import { Product } from "../entities/product.entity";
 
 @Controller('v1/products')
 export class ProductController {
@@ -14,7 +15,37 @@ export class ProductController {
             const products = await this.service.get();
             return new ResultDto(null, true, products, null);
         } catch (error) {
-            throw new HttpException(new ResultDto('Não foi possível obter os produtos', false, null, error), HttpStatus.BAD_REQUEST);
+            throw new HttpException(new ResultDto('Não foi possível obter os cliente', false, null, error), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @Post()
+    async post(@Body() model: Product) {
+        try {
+            await this.service.post(model);
+            return new ResultDto(null, true, model, null);
+        } catch (error) {
+            throw new HttpException(new ResultDto('Não foi possível adicionar o produto', false, null, error), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @Put(':id')
+    async put(@Param('id') id: number, @Body() model: Product) {
+        try {
+            await this.service.put(id, model);
+            return new ResultDto(null, true, model, null);
+        } catch (error) {
+            throw new HttpException(new ResultDto('Não foi possível atualizar o produto', false, null, error), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @Delete(':id')
+    async delete(@Param('id') id: number) {
+        try {
+            await this.service.delete(id);
+            return new ResultDto('O produto foi removido com sucesso', true, null, null);
+        } catch (error) {
+            throw new HttpException(new ResultDto('Não foi possível remover o produto', false, null, error), HttpStatus.BAD_REQUEST);
         }
     }
 }
