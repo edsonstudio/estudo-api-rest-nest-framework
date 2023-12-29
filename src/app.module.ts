90eb5f38624ff1a0a3c68ab68a6cd6722888e32e
edsonstudio <edsont8@hotmail.com>
2023-12-29 11:11:32 -0300

    Substituido o client mySql pelo MySql2, importado o modulo do TypeORM e configurada a string de conexao no AppModule;

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,24 @@
 import { Module } from '@nestjs/common';
-import { BackofficeModule } from './modules/backoffice/backoffice.module';
+
 import { MongooseModule } from '@nestjs/mongoose';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { BackofficeModule } from './modules/backoffice/backoffice.module';
 import { StoreModule } from './modules/store/store.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb+srv://edsonstudio:rud6JHpd8oqJJukV@cluster0.d8g8xgw.mongodb.net'),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '127.0.0.1',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'db_nest_store',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
     BackofficeModule,
     StoreModule
   ],
