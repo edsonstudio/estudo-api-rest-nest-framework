c09742e2730aa45e292a4b6576885bd54aaa0954
edsonstudio <edsont8@hotmail.com>
2023-12-29 10:43:15 -0300

    Criacao do modulo de store, instalacao das bibliotecas necessarias para utilizar o banco de dados MySql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { BackofficeModule } from './modules/backoffice/backoffice.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { StoreModule } from './modules/store/store.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb+srv://edsonstudio:rud6JHpd8oqJJukV@cluster0.d8g8xgw.mongodb.net'),
-    BackofficeModule
+    BackofficeModule,
+    StoreModule
   ],
   controllers: [],
   providers: [],
