50b5d2a2e931dfe8da8f96182c5384567b1d5484
edsonstudio <edsont8@hotmail.com>
2024-01-02 11:24:14 -0300

    Adicionado o pacote @nestjs/config para utilizar variaveis de ambiente, criado o arquivo .env contendo as variaveis de ambiente utilizadas na aplicacao, ajustado o arquivo main.ts para utilizar as funcionalidades do nest/config, substituidos os valores da porta da aplicacao e as strings de conexao com os bancos de dados por variaveis de ambiente

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
@@ -8,16 +9,38 @@ import { StoreModule } from './modules/store/store.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb+srv://edsonstudio:rud6JHpd8oqJJukV@cluster0.d8g8xgw.mongodb.net'),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: '127.0.0.1',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'db_nest_store',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    // MongooseModule.forRoot('mongodb+srv://edsonstudio:rud6JHpd8oqJJukV@cluster0.d8g8xgw.mongodb.net'), // Versão sem o Nest Config
+    // TypeOrmModule.forRoot({  // Versão sem o Nest Config
+    //   type: 'mysql',
+    //   host: '127.0.0.1',
+    //   port: 3306,
+    //   username: 'root',
+    //   password: 'root',
+    //   database: 'db_nest_store',
+    //   entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    //   synchronize: true,
+    // }),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_CONNECTION_STRING'),
+      }),
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DATABASE_HOST'),
+        port: configService.get<number>('DATABASE_PORT'),
+        username: configService.get<string>('DATABASE_USERNAME'),
+        password: configService.get<string>('DATABASE_PASSWORD'),
+        database: configService.get<string>('DATABASE_DATABASE'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
     }),
     BackofficeModule,
     StoreModule
@@ -25,4 +48,4 @@ import { StoreModule } from './modules/store/store.module';
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
