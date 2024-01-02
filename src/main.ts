6fff8e521a65d4783aad1e5a695c7a84fdddc479
edsonstudio <edsont8@hotmail.com>
2024-01-02 14:22:55 -0300

    Adicionado o pacote compression e implementado de forma global no arquivo main.ts para que seja feita a compressao dos dados de todos os responses

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { CustomLogger } from './shared/services/custom-logger.service';
+import * as compression from 'compression';
 
 async function bootstrap() {
   // Opção para ter o log da aplicação no console e de forma customizada:
@@ -11,6 +12,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT');
+  app.use(compression());
   await app.listen(port);
 }
 bootstrap();
